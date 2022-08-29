055489aefd902afb9ae1956b1e47004b3cfaca3a
marianaabento <mariibentoo04@gmail.com>
2022-08-29 11:43:29 -0300

    aula_6

---
+++ core/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Filmes
+
+class FilmesSerializer(ModelSerializer):
+    class Meta:
+        model = Filmes
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Filmes
+from core.serializers import FilmesSerializer
+
+class FilmesViewSet(ModelViewSet):
+    queryset = Filmes.objects.all()
+    serializer_class = FilmesSerializer
