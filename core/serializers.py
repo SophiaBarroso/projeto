0aa8804ab5300c645545c28a06de8e87a51f8b75
marianaabento <mariibentoo04@gmail.com>
2022-08-29 11:51:20 -0300

    alterações2

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Filmes
+from core.models import Genero
 
 class FilmesSerializer(ModelSerializer):
     class Meta:
         model = Filmes
+        fields = "__all__"
+        
+class GeneroSerializer(ModelSerializer):
+    class Meta:
+        model = Genero
+        fields = "__all__"
