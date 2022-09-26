b26d07ffdcd574b9a44b0bea6c9eca63d626e396
marianaabento <mariibentoo04@gmail.com>
2022-09-26 16:28:19 -0300

    pasta tests

--- core/admin/plataforma.py
+++ core/admin/plataforma.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from core.models import Plataforma
 
 class PlataformaAdmin(admin.ModelAdmin):
-    list_display:("plataforma",)
+    list_display = ("plataforma")
 
 admin.site.register(Plataforma, PlataformaAdmin)
