7dcfa2563dfc78648a77fcfb2ec46954d9fbf7d9
marianaabento <mariibentoo04@gmail.com>
2022-09-28 16:38:24 -0300

    adaptações

--- core/admin/critica.py
+++ core/admin/critica.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from core.models import Critica
 
 class CriticaAdmin(admin.ModelAdmin):
-    list_display = ("conteudo", "crt_user", "crt_filme")
+    list_display = ("conteudo", "crt_user", "crt_filme", "nota")
 
 admin.site.register(Critica, CriticaAdmin)
