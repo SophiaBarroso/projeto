feacb1162756f829cf0d9e1c1692642b3c0c76f6
marianaabento <mariibentoo04@gmail.com>
2022-09-28 15:56:03 -0300

    nota critica

--- core/models/critica.py
+++ core/models/critica.py
@@ -4,7 +4,15 @@ from core.models.filmes import Filmes
 from core.models.usuario import Usuario
 
 class Critica(models.Model):
+    estrelas = (
+        (1, '1 estrelas'),
+        (2, "2 estrelas"),
+        (3, '3 estrelas'),
+        (4, "4 estrelas"),
+        (5, "5 estrelas"),
+    )
     conteudo = models.CharField(max_length=125)
     dt_critica = models.DateField()
     crt_user = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='criticas')
     crt_filme = models.ForeignKey(Filmes, on_delete=models.PROTECT, related_name='criticas')
+    nota = models.PositiveIntegerField(null=False, choices=estrelas)
