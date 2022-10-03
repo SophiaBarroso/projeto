f114721ae01b4b625c439f5e8703db02ef47252a
marianaabento <mariibentoo04gmail.com>
2022-10-03 11:29:37 -0300

    adaptação

--- core/admin/elenco.py
+++ core/admin/elenco.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from core.models import Elenco
 
 class ElencoAdmin (admin.ModelAdmin):
-    list_display = ("artista", "personagem")
+    list_display = ("nome", "personagem")
 
 admin.site.register(Elenco, ElencoAdmin)
--- core/models/elenco.py
+++ core/models/elenco.py
@@ -1,12 +1,12 @@
 from django.db import models
-from core.models.elenco import Elenco
+from core.models.filmes import Filmes
 
 from .artistas import Artistas
 
 class Elenco(models.Model):
     personagem = models.CharField(max_length=50)
-    nome = models.ForeignKey(Artistas, related_name="Elenco")
-    titulo = models.ManyToManyField(Filmes, related_name="Artistas")
+    nome = models.ForeignKey(Artistas, on_delete=models.PROTECT,related_name="Elenco")
+    titulo = models.ManyToManyField(Filmes, related_name="Elenco")
     
     def __str__(self):
         return self.personagem
